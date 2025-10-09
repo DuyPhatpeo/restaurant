b9bd9b62f4abbf54a5c8b1a2edd65e44a7fd4a4c
Tran Duy Phat <phattranduy00@gmail.com>
2025-10-09 16:42:07 +0700

    fix time

--- src/lib/utils/bookingRules.js
+++ src/lib/utils/bookingRules.js
@@ -1,6 +1,6 @@
 // @lib/utils/bookingRules.js
 
-export const BLOCKED_TIME_RANGES = [{ start: [16, 0], end: [9, 0] }];
+export const BLOCKED_TIME_RANGES = [{ start: [20, 0], end: [9, 0] }];
 export const BLOCKED_DAYS = [0, 6];
 
 // Trạng thái booking: "available" | "full"
