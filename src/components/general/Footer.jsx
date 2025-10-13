3193d28cbf1553010e5918c56168ab1847247aec
Tran Duy Phat <phattranduy00@gmail.com>
2025-10-13 16:00:12 +0700

    update code

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -103,10 +103,7 @@ const Footer = () => {
 
       {/* Copyright */}
       <div className="copyright">
-        Copyright ©2025 All rights reserved | Made with ❤️ by Dino |{" "}
-        <a href="/sitemap" className="sitemap-link">
-          Website Map
-        </a>
+        Copyright ©2025 All rights reserved | Made with ❤️ by Dino
       </div>
     </footer>
   );
